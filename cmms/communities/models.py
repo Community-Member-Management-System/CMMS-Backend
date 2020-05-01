b65df8199bbc605de056532e7e3b11bbc225c8af
kalineid <noob@mail.ustc.edu.cn>
2020-05-01 15:03:31 +0800

    Add verbose name
    Add related name for foreign keys

--- cmms/communities/models.py
+++ cmms/communities/models.py
@@ -7,37 +7,42 @@ from django.utils import timezone
 # Create your models here.
 class Community(models.Model):
     creator = models.ForeignKey(
-        _('创建者'),
         settings.AUTH_USER_MODEL,
-        on_delele=models.SET_NULL,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name=_('创建者'),
+        related_name='communities_created',
     )
     # community can be transferred, so owner and creator may differ
     owner = models.ForeignKey(
-        _('拥有者'),
         settings.AUTH_USER_MODEL,
-        on_delte=models.SET_NULL,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name=_('拥有者'),
+        related_name='communities_owned',
     )
-    name = models.CharField(_('社团名称'), max_length=64)
-    profile = models.TextField(_('社团简介'), blank=True)
-    date_created = models.DateTimeField(_('创建时间'), default=timezone.now)
+    name = models.CharField(max_length=64, verbose_name=_('社团名称'))
+    profile = models.TextField(blank=True, verbose_name=_('社团简介'))
+    date_created = models.DateTimeField(default=timezone.now, verbose_name=_('创建时间'))
     members = models.ManyToManyField(
-        _('成员'),
         settings.AUTH_USER_MODEL,
         through='Membership',
         through_fields=('community', 'user'),
+        verbose_name=_('成员'),
+        related_name='communities_joined',
     )
-    admins = models.ManyToManyField(_('管理员'), settings.AUTH_USER_MODEL)
+    admins = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_('管理员'))
 
 
 class Membership(models.Model):
     user = models.ForeignKey(
-        _('用户'),
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        verbose_name=_('用户'),
     )
     community = models.ForeignKey(
-        _('社团'),
-        settings.AUTH_USER_MODEL,
+        Community,
         on_delete=models.CASCADE,
+        verbose_name=_('社团'),
     )
-    date_joined = models.DateTimeField(_('加入时间'), auto_now_add=True)
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name=_('加入时间'))
