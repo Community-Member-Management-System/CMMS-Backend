fad7f4b789882a1d6d71712f04345c526d5ca2b4
taoky <taoky1999@gmail.com>
2020-05-01 15:55:33 +0800

    fix django test in CI

--- cmms/account/tests.py
+++ cmms/account/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import User
 
-# Create your tests here.
+
+class LoginAndLogoutTests(APITestCase):
+    def setUp(self):
+        User.objects.create(gid="test", student_id="test", password="test")
+
+    def test_traditional_login(self):
+        # unfinished
+        url = '/api/auth/login/'
+        data = {'username': 'test', 'password': 'test'}
+
+        response = self.client.post(url, data)
+        print(response.content)
