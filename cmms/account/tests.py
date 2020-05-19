6f84d6f74bb8aaa42ef7fd3c5bf619c90c5f4756
taoky <taoky1999@gmail.com>
2020-05-19 14:52:13 +0800

    Account: update tests

--- cmms/account/tests.py
+++ cmms/account/tests.py
@@ -6,14 +6,23 @@ from .models import User
 class LoginAndLogoutTests(APITestCase):
     def setUp(self):
         User.objects.create_user(gid="testgid", student_id="teststuid", password="test")
+        User.objects.create_user(gid="gid2", student_id="PB23333333", password="test2", nick_name="myname")
 
-    def test_traditional_login(self):
+    def test_traditional_login_new_user(self):
         url = '/api/auth/traditional_login'
         data = {'username': 'teststuid', 'password': 'test'}
 
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["new"], True)
+
+    def test_traditional_login_old_user(self):
+        url = '/api/auth/traditional_login'
+        data = {'username': 'PB23333333', 'password': 'test2'}
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["new"], False)
 
     def test_failed_traditional_login(self):
         url = '/api/auth/traditional_login'
