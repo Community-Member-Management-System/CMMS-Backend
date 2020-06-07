906a1b9aeb28fe4882746e5352afa2c8cb0025d9
Neopolitan <neopolitan@mail.ustc.edu.cn>
2020-06-07 19:52:07 +0800

    notice: try to deprecate enum and turn to choice

--- cmms/notice/__init__.py
+++ cmms/notice/__init__.py
@@ -1,3 +1,3 @@
-from enum import Enum
-
-NoticeType = Enum('NoticeType', ('pc', 'ar', 'ca', 'b', 'c_an', 'c_ap', 'c_aa', 'c_d', 's_ca'))
+# from enum import Enum
+#
+# NoticeType = Enum('NoticeType', ('pc', 'ar', 'ca', 'b', 'c_an', 'c_ap', 'c_aa', 'c_d', 's_ca'))
--- cmms/notice/models.py
+++ cmms/notice/models.py
@@ -2,12 +2,26 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from . import NoticeType
+# from . import NoticeType
 
 
 class Notice(models.Model):
+    NOTICE_TYPE_CHOICES = [
+        ('PC', 'PersonalCommunalNotice'),
+        ('AR', 'At/ReplyNotice'),
+        ('CA', 'Create/ApplyNotice'),
+        ('B', 'BanNotice'),
+        ('C_AN', 'Community_ActivityNotice'),
+        ('C_AP', 'Community_AdminPersonnelNotice'),
+        ('C_AA', 'Community_AdminAuditNotice'),
+        ('C_D', 'Community_DismissNotice'),
+        ('S_CA', 'System_CommunityAuditNotice'),
+    ]
+
     date = models.DateTimeField(default=timezone.now, verbose_name=_('通知时间'))
-    type = models.IntegerField(verbose_name=_('通知类型'))
+    type = models.CharField(max_length=4,
+                            choices=NOTICE_TYPE_CHOICES,
+                            verbose_name=_('通知类型'))
     related_user = models.ForeignKey(settings.AUTH_USER_MODEL,
                                      on_delete=models.CASCADE,
                                      verbose_name=_('关联用户'),
