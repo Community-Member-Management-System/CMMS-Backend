92608efacbb22eea77e88523043d2e1bd0a53aca
taoky <taoky1999@gmail.com>
2020-08-01 13:34:51 +0800

    add permission_classes for NoticeView

--- cmms/notice/views.py
+++ cmms/notice/views.py
@@ -1,3 +1,4 @@
+from account.utils import ValidUserPermission
 from .serializers import NoticeSerializer, NoticeBoxSerializer
 from .models import Notice, NoticeBox
 from .utils import NoticeManager
@@ -9,30 +10,26 @@ from django.utils import timezone
 
 
 class NoticeView(APIView):
+    permission_classes = [ValidUserPermission]
+
     def get(self, request, format=None):
         user = request.user
-        if user.is_authenticated:
-            notice_list = NoticeManager.fetch(user)
-            serializer = NoticeBoxSerializer(notice_list, many=True)
-            return Response(serializer.data)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        notice_list = NoticeManager.fetch(user)
+        serializer = NoticeBoxSerializer(notice_list, many=True)
+        return Response(serializer.data)
 
     def post(self, request, format=None):
         user = request.user
         pk = request.data.get('pk')
         method = request.data.get('method')
-        if user.is_authenticated:
-            if method == 'read':
-                NoticeManager.read(user, pk)
-            elif method == 'unread':
-                NoticeManager.unread(user, pk)
-            elif method == 'delete':
-                NoticeManager.delete(user, pk)
-            else:
-                notice = NoticeManager.access(user, pk)
-                serializer = NoticeSerializer(notice)
-                return Response(serializer.data)
+        if method == 'read':
+            NoticeManager.read(user, pk)
+        elif method == 'unread':
+            NoticeManager.unread(user, pk)
+        elif method == 'delete':
+            NoticeManager.delete(user, pk)
         else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+            notice = NoticeManager.access(user, pk)
+            serializer = NoticeSerializer(notice)
+            return Response(serializer.data)
         return Response(status=status.HTTP_200_OK)
