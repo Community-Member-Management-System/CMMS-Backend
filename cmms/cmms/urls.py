5f19d94dae06eedf5e305b10db3343be93729d1f
taoky <taoky1999@gmail.com>
2020-08-02 15:19:30 +0800

    fix CI

--- cmms/cmms/urls.py
+++ cmms/cmms/urls.py
@@ -23,15 +23,13 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title='CMMS API',
-      default_version='v1',
-      description='CMMS Backend',
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(title='CMMS API',
+                 default_version='v1',
+                 description='CMMS Backend',
+                 ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
-
 
 urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
