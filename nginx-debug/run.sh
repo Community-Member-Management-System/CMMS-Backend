e6d04c083cb3bca97c35e0d81c5f485cf98f5243
iBug <git@ibugone.com>
2020-08-02 15:57:27 +0800

    Add Dockerfile
    You know what I mean :)

--- nginx-debug/run.sh
+++ nginx-debug/run.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+cd "$(basename "$0")"
 echo "Remember to 'npm run serve' and 'python manage.py runserver'!"
 echo "Now nginx will start..."
-nginx -p `pwd`/ -c conf/nginx.conf && echo "[OK], listening to localhost:1234"
+nginx -p $PWD/ -c conf/nginx.conf && echo "[OK], listening to localhost:1234"
