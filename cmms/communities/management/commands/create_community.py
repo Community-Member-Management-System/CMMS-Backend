1454fd1a2cbf9a8e9d712a82712b00e26af3fb97
taoky <taoky1999@gmail.com>
2020-08-05 15:44:18 +0800

    communities: start writing tests

--- cmms/communities/management/commands/create_community.py
+++ cmms/communities/management/commands/create_community.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+
 from communities.models import Community
 from account.models import User
 
@@ -18,15 +20,20 @@ class Command(BaseCommand):
         user_id = options['user_id']
         valid = bool(options['valid'])
 
+        user = User.objects.get(id=user_id)
+
         for i in range(int_from, int_to + 1):
             sid = f'club{i}'
-            Community.objects.create(
-                creator=User.objects.get(id=user_id),
-                owner=User.objects.get(id=user_id),
-                name=sid,
-                profile=sid + 'profile',
-                valid=valid
-            )
+            with transaction.atomic():
+                c = Community.objects.create(
+                    creator=user,
+                    owner=user,
+                    name=sid,
+                    profile=sid + 'profile',
+                    valid=valid
+                )
+                c.admins.add(user)
+                c.members.add(user, through_defaults={'valid': True})
             self.stdout.write(
                 f'Community (name {sid}) created.'
             )
--- cmms/communities/tests.py
+++ cmms/communities/tests.py
@@ -5,17 +5,25 @@ from .models import Community
 
 
 class CommunitiesTests(APITestCase):
+    def create_community(self, user: User, name: str, profile: str = '', valid: bool = True):
+        c: Community = Community.objects.create(creator=user, owner=user, name=name,
+                                                profile=profile, valid=valid)
+        c.admins.add(user)
+        c.members.add(user, through_defaults={'valid': True})  # type: ignore
+        return c
+
     def setUp(self):
         self.user1 = User.objects.create_user(gid="testgid", student_id="teststuid", password="test",
                                               profile="testprofile", nick_name="user1", real_name="user11")
         self.user2 = User.objects.create_user(gid="gid2", student_id="PB23333333", password="test2",
                                               nick_name="myname", real_name="myname2")
-        self.club1 = Community.objects.create(creator=self.user1, owner=self.user1, name='club1',
-                                              profile='thisisclub1', valid=True)
-        self.club2 = Community.objects.create(creator=self.user2, owner=self.user2, name='club2',
-                                              profile='thisisclub2', valid=True)
+        self.user3 = User.objects.create_user(gid="gid3", student_id="sysadmin", password="sysadmin",
+                                              nick_name="sysadmin", real_name="sysadmin", is_superuser=True)
+        self.club1 = self.create_community(user=self.user1, name='club1', profile='thisisclub1', valid=True)
+        self.club2 = self.create_community(user=self.user2, name='club2', profile='thisisclub2', valid=True)
+        self.club3 = self.create_community(user=self.user3, name='club3', profile='thisisclub3', valid=False)
 
-    def test_list_community(self):
+    def test_list_communities(self):
         url = '/api/community/'
 
         response = self.client.get(url)
@@ -23,3 +31,30 @@ class CommunitiesTests(APITestCase):
         self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data[0]['name'], 'club1')
         self.assertEqual(response.data[0]['profile'], 'thisisclub1')
+
+    def test_retrieve_community(self):
+        url = f'/api/community/{self.club1.id}'
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'club1')
+        self.assertEqual(response.data['join_status'], '')
+
+        self.client.login(username='PB23333333', password='test2')
+
+        url = f'/api/community/{self.club1.id}'
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['join_status'], '未加入')
+
+        url = f'/api/community/{self.club2.id}'
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['join_status'], '已加入')
+
+        url = f'/api/community/{self.club3.id}'
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
