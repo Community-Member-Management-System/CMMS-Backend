b163d05e4397dc9739c0704a3f2f659b67161766
taoky <taoky1999@gmail.com>
2020-08-25 12:18:19 +0800

    activity: add signed_in_list interface

--- cmms/activity/urls.py
+++ cmms/activity/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path('<int:pk>', views.ActivityDetailUpdateDestroyView.as_view(), name='detail_update_destroy'),
     path('<int:pk>/sign_in', views.ActivitySignInView.as_view(), name='sign_in'),
     path('<int:pk>/secret_key', views.ActivitySecretKeyView.as_view(), name='secret_key'),
+    path('<int:pk>/signed_in_list', views.ActivitySignedInViewSet.as_view({
+        'get': 'retrieve'
+    }), name='retrieve_signed_in_list'),
+    path('<int:pk>/signed_in_list/add/<int:user_id>', views.ActivitySignedInViewSet.as_view({
+        'post': 'add'
+    }), name='signed_in_list_add'),
+    path('<int:pk>/signed_in_list/remove/<int:user_id>', views.ActivitySignedInViewSet.as_view({
+        'post': 'remove'
+    }), name='signed_in_list_remove'),
 ]
