be46a333b8f4af8dea7a42984a92a177a74d0bcc
taoky <taoky1999@gmail.com>
2020-08-31 19:34:51 +0800

    activity: allow superuser to do anything

--- cmms/activity/permissions.py
+++ cmms/activity/permissions.py
@@ -6,9 +6,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.related_community.admins.filter(id=request.user.id).exists()
+        return obj.related_community.admins.filter(id=request.user.id).exists() or request.user.is_superuser
 
 
 class IsAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.related_community.admins.filter(id=request.user.id).exists()
+        return obj.related_community.admins.filter(id=request.user.id).exists() or request.user.is_superuser
